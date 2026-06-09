eb772f735f199251d63161881ea917318afb8148
anu129 <anushagupta9297@gmail.com>
2026-06-09 07:58:45 +0530

    data ingestion and trasfomation file, create rocessor.pkl

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,7 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformationConfig, DataTransformer
 
 
 @dataclass #decorator - because 
@@ -44,7 +45,9 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
-
+    train_data, test_data = obj.initiate_data_ingestion()
+    transformer  = DataTransformer()
+    transformer.initiate_data_transformation(train_data, test_data)
+    
 
 
